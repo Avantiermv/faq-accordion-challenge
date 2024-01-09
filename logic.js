a47c11e3567048d92ac7165e8e8dd7c8b001ecd6
marcos vinicius <avantiermv@gmail.com>
2024-01-09 16:33:51 -0300

    .

--- logic.js
+++ logic.js
@@ -1,5 +1,21 @@
-var image = ''
-
 function pressionar1() {
-    
+    var imagem = document.querySelector('#resto div:nth-child(1) p img');//*essse nth-child serve para indentificar se a div é a número 1 ou 2 ou 3, na hierarquia.
+    imagem.src = './assets/images/icon-minus.svg';
 }
+
+function pressionar2() {
+    var imagem = document.querySelector('#resto div:nth-child(2) p img');
+    imagem.src = './assets/images/icon-minus.svg';
+}
+
+function pressionar3() {
+    var imagem = document.querySelector('#resto div:nth-child(3) p img');
+    imagem.src = './assets/images/icon-minus.svg';
+}
+
+function pressionar4() {
+    var imagem = document.querySelector('#resto div:nth-child(4) p img');
+    imagem.src = './assets/images/icon-minus.svg';
+}
+
+
