0cf472a59a0e158494c76923959bd5c5790b9491
marcos vinicius <avantiermv@gmail.com>
2024-01-12 13:52:24 -0300

    .

--- logic.js
+++ logic.js
@@ -1,33 +1,53 @@
 function pressionar1() {
-    esconderDivs();
     let div = document.querySelector('div:nth-child(2) .esconder');
-    div.style.display = 'block';
     let imagem = document.querySelector('div:nth-child(2) p img');
-    imagem.src = './assets/images/icon-minus.svg';
+    if (div.style.display === 'block') {
+        div.style.display = 'none';
+        imagem.src = './assets/images/icon-plus.svg';
+    } else {
+        esconderDivs();
+        div.style.display = 'block';
+        imagem.src = './assets/images/icon-minus.svg';
+    }
 }
 
 function pressionar2() {
-    esconderDivs();
     let div = document.querySelector('div:nth-child(3) .esconder');
-    div.style.display = 'block';
     var imagem = document.querySelector('div:nth-child(3) p img');
-    imagem.src = './assets/images/icon-minus.svg';
+    if (div.style.display === 'block') {
+        div.style.display = 'none';
+        imagem.src = './assets/images/icon-plus.svg';
+    } else {
+        esconderDivs();
+        div.style.display = 'block';
+        imagem.src = './assets/images/icon-minus.svg';
+    }
 }
 
 function pressionar3() {
-    esconderDivs();
     let div = document.querySelector('div:nth-child(4) .esconder');
-    div.style.display = 'block';
     var imagem = document.querySelector('div:nth-child(4) p img');
-    imagem.src = './assets/images/icon-minus.svg';
+    if (div.style.display === 'block') {
+        div.style.display = 'none';
+        imagem.src = './assets/images/icon-plus.svg';
+    } else {
+        esconderDivs();
+        div.style.display = 'block';
+        imagem.src = './assets/images/icon-minus.svg';
+    }
 }
 
 function pressionar4() {
-    esconderDivs();
     let div = document.querySelector('div:nth-child(5) .esconder');
-    div.style.display = 'block';
     var imagem = document.querySelector('div:nth-child(5) p img');
-    imagem.src = './assets/images/icon-minus.svg';
+    if (div.style.display === 'block') {
+        div.style.display = 'none';
+        imagem.src = './assets/images/icon-plus.svg';
+    } else {
+        esconderDivs();
+        div.style.display = 'block';
+        imagem.src = './assets/images/icon-minus.svg';
+    }
 }
 
 function esconderDivs() {
@@ -38,8 +58,8 @@ function esconderDivs() {
         let imagem = document.querySelector('div:nth-child(' + (i + 2) + ') p img');
         imagem.src = './assets/images/icon-plus.svg';
     }
-   
 }
+
 
 
 
