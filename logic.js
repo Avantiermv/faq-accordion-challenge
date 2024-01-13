bbf1b488ca78c3337f6426dc51b9428b01e64fa1
marcos vinicius <avantiermv@gmail.com>
2024-01-13 14:54:54 -0300

    .

--- logic.js
+++ logic.js
@@ -1,22 +1,44 @@
+var estado1 = false;
+var estado2 = false;
+
 
 function pressionar1(){
-    let imagem = document.querySelector('div:nth-child(1) > p > img');
-    imagem.src = ('./assets/images/icon-minus.svg');
+    var estado1 = true
+    let imagem = document.querySelector('.todotexto:nth-of-type(1) > .paragrafos > p > img');
+    imagem.src = './assets/images/icon-minus.svg'
+
+    let texto = document.querySelector('.todotexto:nth-of-type(1) > .paragrafos > .esconder');
+    texto.style.display = 'block';
+
+    
+
 }
 
 function pressionar2(){
-    let imagem = document.querySelector('div:nth-child(2) > p > img');
-    imagem.src = ('./assets/images/icon-minus.svg');
+    let imagem = document.querySelector('.todotexto:nth-of-type(2) .paragrafos p img');
+    imagem.src = './assets/images/icon-minus.svg';
+
+    let texto = document.querySelector('.todotexto:nth-of-type(2) > .paragrafos > .esconder');
+    texto.style.display = 'block';
 }
 
+
 function pressionar3(){
-    let imagem = document.querySelector('div > p > img');
+    let imagem = document.querySelector('.todotexto:nth-of-type(3) .paragrafos p img');
     imagem.src = ('./assets/images/icon-minus.svg');
+
+    let texto = document.querySelector('.todotexto:nth-of-type(3) > .paragrafos > .esconder');
+    texto.style.display = 'block';
 }
 
 function pressionar4(){
-    let imagem = document.querySelector('div > p > img');
+    let imagem = document.querySelector('.todotexto:nth-of-type(4) .paragrafos p img');
     imagem.src = ('./assets/images/icon-minus.svg');
+
+    let texto = document.querySelector('.todotexto:nth-of-type(4) > .paragrafos > .esconder');
+    texto.style.display = 'block';
+
+    
 }
 
 
