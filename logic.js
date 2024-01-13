e4674ab122ca4377b26273e714761b6d12ed54f4
marcos vinicius <avantiermv@gmail.com>
2024-01-13 11:09:04 -0300

    .

--- logic.js
+++ logic.js
@@ -1,4 +1,23 @@
 
+function pressionar1(){
+    let imagem = document.querySelector('div > p > img');
+    imagem.src = ('./assets/images/icon-minus.svg');
+}
+
+function pressionar2(){
+    let imagem = document.querySelector('div > p > img');
+    imagem.src = ('./assets/images/icon-minus.svg');
+}
+
+function pressionar3(){
+    let imagem = document.querySelector('div > p > img');
+    imagem.src = ('./assets/images/icon-minus.svg');
+}
+
+function pressionar4(){
+    let imagem = document.querySelector('div > p > img');
+    imagem.src = ('./assets/images/icon-minus.svg');
+}
 
 
 
