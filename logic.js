2b93c037848cdf9f7cb5c9f42a643114852560a5
marcos vinicius <avantiermv@gmail.com>
2024-01-17 14:36:04 -0300

    .

--- logic.js
+++ logic.js
@@ -1,6 +1,7 @@
 function pressionar1(){
     
-   let div+ so
+    let divtodo1 = document.querySelector('.todotexto:nth-of-type(1)');
+    divtodo1.style.height = '126px';
 
     let imagem1 = document.querySelector('.todotexto:nth-of-type(1) > .paragrafos > p > img');
     imagem1.src = './assets/images/icon-minus.svg';
@@ -8,10 +9,9 @@ function pressionar1(){
     let texto1 = document.querySelector('.todotexto:nth-of-type(1) > .paragrafos > .esconder');
     texto1.style.display = 'block';
 
-    
-
     //* pressionar2
-    
+    let divtodo2 = document.querySelector('.todotexto:nth-of-type(2)');
+    divtodo2.style.height = '80px';
 
     let imagem2 = document.querySelector('.todotexto:nth-of-type(2) .paragrafos p img');
     imagem2.src = './assets/images/icon-plus.svg';
@@ -19,12 +19,31 @@ function pressionar1(){
     let texto2 = document.querySelector('.todotexto:nth-of-type(2) > .paragrafos > .esconder');
     texto2.style.display = 'none';
 
-    
+    //*pressionar3
+    let divtodo3 = document.querySelector('.todotexto:nth-of-type(3)');
+    divtodo3.style.height = '80px';
+
+    let imagem3 = document.querySelector('.todotexto:nth-of-type(3) .paragrafos p img');
+    imagem3.src = './assets/images/icon-plus.svg';
+
+    let texto3 = document.querySelector('.todotexto:nth-of-type(3) > .paragrafos > .esconder');
+    texto3.style.display = 'none';
+
+    //*pressionar4
+    let divtodo4 = document.querySelector('.todotexto:nth-of-type(4)');
+    divtodo4.style.height = '80px';
+
+    let imagem4 = document.querySelector('.todotexto:nth-of-type(4) .paragrafos p img');
+    imagem4.src = './assets/images/icon-plus.svg';
+
+    let texto4 = document.querySelector('.todotexto:nth-of-type(4) > .paragrafos > .esconder');
+    texto4.style.display = 'none';
 }
 
 function pressionar2(){
     
-   
+    let divtodo2 = document.querySelector('.todotexto:nth-of-type(2)');
+    divtodo2.style.height = '126px';
 
     let imagem2 = document.querySelector('.todotexto:nth-of-type(2) .paragrafos p img');
     imagem2.src = './assets/images/icon-minus.svg';
@@ -33,124 +52,115 @@ function pressionar2(){
     texto2.style.display = 'block';
     
     //* pressionar1
-    
+    let divtodo1 = document.querySelector('.todotexto:nth-of-type(1)');
+    divtodo1.style.height = '80px';
 
     let imagem1 = document.querySelector('.todotexto:nth-of-type(1) > .paragrafos > p > img');
     imagem1.src = './assets/images/icon-plus.svg';
 
     let texto1 = document.querySelector('.todotexto:nth-of-type(1) > .paragrafos > .esconder');
     texto1.style.display = 'none';   
+
+    //*pressionar3
+    let divtodo3 = document.querySelector('.todotexto:nth-of-type(3)');
+    divtodo3.style.height = '80px';
+
+    let imagem3 = document.querySelector('.todotexto:nth-of-type(3) .paragrafos p img');
+    imagem3.src = './assets/images/icon-plus.svg';
+
+    let texto3 = document.querySelector('.todotexto:nth-of-type(3) > .paragrafos > .esconder');
+    texto3.style.display = 'none';
+
+    //*pressionar4
+    let divtodo4 = document.querySelector('.todotexto:nth-of-type(4)');
+    divtodo4.style.height = '80px';
+
+    let imagem4 = document.querySelector('.todotexto:nth-of-type(4) .paragrafos p img');
+    imagem4.src = './assets/images/icon-plus.svg';
+
+    let texto4 = document.querySelector('.todotexto:nth-of-type(4) > .paragrafos > .esconder');
+    texto4.style.display = 'none';
 }
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 function pressionar3(){
+    let divtodo3 = document.querySelector('.todotexto:nth-of-type(3)');
+    divtodo3.style.height = '126px';
+
     let imagem3 = document.querySelector('.todotexto:nth-of-type(3) .paragrafos p img');
-    imagem3.src = ('./assets/images/icon-minus.svg');
+    imagem3.src = './assets/images/icon-minus.svg';
 
     let texto3 = document.querySelector('.todotexto:nth-of-type(3) > .paragrafos > .esconder');
     texto3.style.display = 'block';
+    
+    //* pressionar1
+    let divtodo1 = document.querySelector('.todotexto:nth-of-type(1)');
+    divtodo1.style.height = '80px';
 
-     //* pressionar1
-     let imagem1 = document.querySelector('.todotexto:nth-of-type(1) > .paragrafos > p > img');
-     imagem1.src = './assets/images/icon-plus.svg';
- 
-     let texto1 = document.querySelector('.todotexto:nth-of-type(1) > .paragrafos > .esconder');
-     texto1.style.display = 'none';
- 
-      //* pressionar2
-      let imagem2 = document.querySelector('.todotexto:nth-of-type(2) .paragrafos p img');
-      imagem2.src = './assets/images/icon-plus.svg';
-      
-      let texto2 = document.querySelector('.todotexto:nth-of-type(2) > .paragrafos > .esconder');
-      texto2.style.display = 'none';
- 
-       //* pressionar4
-       let imagem4 = document.querySelector('.todotexto:nth-of-type(4) .paragrafos p img');
-       imagem4.src = './assets/images/icon-plus.svg';
-       
-       let texto4 = document.querySelector('.todotexto:nth-of-type(4) > .paragrafos > .esconder');
-       texto4.style.display = 'none';
+    let imagem1 = document.querySelector('.todotexto:nth-of-type(1) > .paragrafos > p > img');
+    imagem1.src = './assets/images/icon-plus.svg';
+
+    let texto1 = document.querySelector('.todotexto:nth-of-type(1) > .paragrafos > .esconder');
+    texto1.style.display = 'none'; 
+
+    //* pressionar2
+    let divtodo2 = document.querySelector('.todotexto:nth-of-type(2)');
+    divtodo2.style.height = '80px';
+
+    let imagem2 = document.querySelector('.todotexto:nth-of-type(2) .paragrafos p img');
+    imagem2.src = './assets/images/icon-plus.svg';
+    
+    let texto2 = document.querySelector('.todotexto:nth-of-type(2) > .paragrafos > .esconder');
+    texto2.style.display = 'none';
+
+    //*pressionar4
+    let divtodo4 = document.querySelector('.todotexto:nth-of-type(4)');
+    divtodo4.style.height = '80px';
+
+    let imagem4 = document.querySelector('.todotexto:nth-of-type(4) .paragrafos p img');
+    imagem4.src = './assets/images/icon-plus.svg';
+
+    let texto4 = document.querySelector('.todotexto:nth-of-type(4) > .paragrafos > .esconder');
+    texto4.style.display = 'none';
 }
 
 function pressionar4(){
+    let divtodo4 = document.querySelector('.todotexto:nth-of-type(4)');
+    divtodo4.style.height = '126px';
+
     let imagem4 = document.querySelector('.todotexto:nth-of-type(4) .paragrafos p img');
-    imagem4.src = ('./assets/images/icon-minus.svg');
+    imagem4.src = './assets/images/icon-minus.svg';
 
     let texto4 = document.querySelector('.todotexto:nth-of-type(4) > .paragrafos > .esconder');
     texto4.style.display = 'block';
 
-     //* pressionar1
-     let imagem1 = document.querySelector('.todotexto:nth-of-type(1) > .paragrafos > p > img');
-     imagem1.src = './assets/images/icon-plus.svg';
- 
-     let texto1 = document.querySelector('.todotexto:nth-of-type(1) > .paragrafos > .esconder');
-     texto1.style.display = 'none';
- 
-      //* pressionar3
-      let imagem3 = document.querySelector('.todotexto:nth-of-type(3) .paragrafos p img');
-      imagem3.src = './assets/images/icon-plus.svg';
-      
-      let texto3 = document.querySelector('.todotexto:nth-of-type(3) > .paragrafos > .esconder');
-      texto3.style.display = 'none';
- 
-       //* pressionar2
-       let imagem2 = document.querySelector('.todotexto:nth-of-type(2) .paragrafos p img');
-       imagem2.src = './assets/images/icon-plus.svg';
-       
-       let texto2 = document.querySelector('.todotexto:nth-of-type(2) > .paragrafos > .esconder');
-       texto2.style.display = 'none';
+    //* pressionar1
+    let divtodo1 = document.querySelector('.todotexto:nth-of-type(1)');
+    divtodo1.style.height = '80px';
 
+    let imagem1 = document.querySelector('.todotexto:nth-of-type(1) > .paragrafos > p > img');
+    imagem1.src = './assets/images/icon-plus.svg';
+
+    let texto1 = document.querySelector('.todotexto:nth-of-type(1) > .paragrafos > .esconder');
+    texto1.style.display = 'none';
+
+    //* pressionar2
+    let divtodo2 = document.querySelector('.todotexto:nth-of-type(2)');
+    divtodo2.style.height = '80px';
+
+    let imagem2 = document.querySelector('.todotexto:nth-of-type(2) .paragrafos p img');
+    imagem2.src = './assets/images/icon-plus.svg';
     
+    let texto2 = document.querySelector('.todotexto:nth-of-type(2) > .paragrafos > .esconder');
+    texto2.style.display = 'none';
+
+    //*pressionar3
+    let divtodo3 = document.querySelector('.todotexto:nth-of-type(3)');
+    divtodo3.style.height = '80px';
+
+    let imagem3 = document.querySelector('.todotexto:nth-of-type(3) .paragrafos p img');
+    imagem3.src = './assets/images/icon-plus.svg';
+
+    let texto3 = document.querySelector('.todotexto:nth-of-type(3) > .paragrafos > .esconder');
+    texto3.style.display = 'none';
+
 }
-
-
-
-
