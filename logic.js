043e0ba9bbac1683918a3483e6225e1e9b82dea3
marcos vinicius <avantiermv@gmail.com>
2024-01-17 09:25:50 -0300

    .

--- logic.js
+++ logic.js
@@ -1,52 +1,122 @@
-var estado1 = false;
-var estado2 = false;
-var estado3 = false;
-var estado4 = false;
-
-
 function pressionar1(){
-    estado1 = true;
-    let imagem = document.querySelector('.todotexto:nth-of-type(1) > .paragrafos > p > img');
-    imagem.src = './assets/images/icon-minus.svg';
-
-    let texto = document.querySelector('.todotexto:nth-of-type(1) > .paragrafos > .esconder');
-    texto.style.display = 'block';
     
-    if (estado1 == true){
-        texto.style.display = 'block';
-    } else if (estado2 == true || estado3 == true){
-        texto.style.display = 'none';
-    }
+    let imagem1 = document.querySelector('.todotexto:nth-of-type(1) > .paragrafos > p > img');
+    imagem1.src = './assets/images/icon-minus.svg';
 
+    let texto1 = document.querySelector('.todotexto:nth-of-type(1) > .paragrafos > .esconder');
+    texto1.style.display = 'block';
+
+    //* pressionar2
+    let imagem2 = document.querySelector('.todotexto:nth-of-type(2) .paragrafos p img');
+    imagem2.src = './assets/images/icon-plus.svg';
+    
+    let texto2 = document.querySelector('.todotexto:nth-of-type(2) > .paragrafos > .esconder');
+    texto2.style.display = 'none';
+
+    //*pressionar3
+    let imagem3 = document.querySelector('.todotexto:nth-of-type(3) .paragrafos p img');
+     imagem3.src = './assets/images/icon-plus.svg';
+     
+     let texto3 = document.querySelector('.todotexto:nth-of-type(3) > .paragrafos > .esconder');
+     texto3.style.display = 'none';
+
+
+    //*pressionar4
+    let imagem4 = document.querySelector('.todotexto:nth-of-type(4) .paragrafos p img');
+     imagem4.src = './assets/images/icon-plus.svg';
+     
+     let texto4 = document.querySelector('.todotexto:nth-of-type(4) > .paragrafos > .esconder');
+     texto4.style.display = 'none';
 }
 
 function pressionar2(){
     estado2 = true;
-    let imagem = document.querySelector('.todotexto:nth-of-type(2) .paragrafos p img');
-    imagem.src = './assets/images/icon-minus.svg';
+    let imagem2 = document.querySelector('.todotexto:nth-of-type(2) .paragrafos p img');
+    imagem2.src = './assets/images/icon-minus.svg';
 
-    let texto = document.querySelector('.todotexto:nth-of-type(2) > .paragrafos > .esconder');
-    texto.style.display = 'block';
-
+    let texto2 = document.querySelector('.todotexto:nth-of-type(2) > .paragrafos > .esconder');
+    texto2.style.display = 'block';
     
+     //* pressionar1
+    let imagem1 = document.querySelector('.todotexto:nth-of-type(1) > .paragrafos > p > img');
+    imagem1.src = './assets/images/icon-plus.svg';
+
+    let texto1 = document.querySelector('.todotexto:nth-of-type(1) > .paragrafos > .esconder');
+    texto1.style.display = 'none';
+
+     //* pressionar3
+     let imagem3 = document.querySelector('.todotexto:nth-of-type(3) .paragrafos p img');
+     imagem3.src = './assets/images/icon-plus.svg';
+     
+     let texto3 = document.querySelector('.todotexto:nth-of-type(3) > .paragrafos > .esconder');
+     texto3.style.display = 'none';
+
+      //* pressionar4
+      let imagem4 = document.querySelector('.todotexto:nth-of-type(4) .paragrafos p img');
+      imagem4.src = './assets/images/icon-plus.svg';
+      
+      let texto4 = document.querySelector('.todotexto:nth-of-type(4) > .paragrafos > .esconder');
+      texto4.style.display = 'none';
     
 }
 
 
 function pressionar3(){
-    let imagem = document.querySelector('.todotexto:nth-of-type(3) .paragrafos p img');
-    imagem.src = ('./assets/images/icon-minus.svg');
+    let imagem3 = document.querySelector('.todotexto:nth-of-type(3) .paragrafos p img');
+    imagem3.src = ('./assets/images/icon-minus.svg');
 
-    let texto = document.querySelector('.todotexto:nth-of-type(3) > .paragrafos > .esconder');
-    texto.style.display = 'block';
+    let texto3 = document.querySelector('.todotexto:nth-of-type(3) > .paragrafos > .esconder');
+    texto3.style.display = 'block';
+
+     //* pressionar1
+     let imagem1 = document.querySelector('.todotexto:nth-of-type(1) > .paragrafos > p > img');
+     imagem1.src = './assets/images/icon-plus.svg';
+ 
+     let texto1 = document.querySelector('.todotexto:nth-of-type(1) > .paragrafos > .esconder');
+     texto1.style.display = 'none';
+ 
+      //* pressionar2
+      let imagem2 = document.querySelector('.todotexto:nth-of-type(2) .paragrafos p img');
+      imagem2.src = './assets/images/icon-plus.svg';
+      
+      let texto2 = document.querySelector('.todotexto:nth-of-type(2) > .paragrafos > .esconder');
+      texto2.style.display = 'none';
+ 
+       //* pressionar4
+       let imagem4 = document.querySelector('.todotexto:nth-of-type(4) .paragrafos p img');
+       imagem4.src = './assets/images/icon-plus.svg';
+       
+       let texto4 = document.querySelector('.todotexto:nth-of-type(4) > .paragrafos > .esconder');
+       texto4.style.display = 'none';
 }
 
 function pressionar4(){
-    let imagem = document.querySelector('.todotexto:nth-of-type(4) .paragrafos p img');
-    imagem.src = ('./assets/images/icon-minus.svg');
+    let imagem4 = document.querySelector('.todotexto:nth-of-type(4) .paragrafos p img');
+    imagem4.src = ('./assets/images/icon-minus.svg');
 
-    let texto = document.querySelector('.todotexto:nth-of-type(4) > .paragrafos > .esconder');
-    texto.style.display = 'block';
+    let texto4 = document.querySelector('.todotexto:nth-of-type(4) > .paragrafos > .esconder');
+    texto4.style.display = 'block';
+
+     //* pressionar1
+     let imagem1 = document.querySelector('.todotexto:nth-of-type(1) > .paragrafos > p > img');
+     imagem1.src = './assets/images/icon-plus.svg';
+ 
+     let texto1 = document.querySelector('.todotexto:nth-of-type(1) > .paragrafos > .esconder');
+     texto1.style.display = 'none';
+ 
+      //* pressionar3
+      let imagem3 = document.querySelector('.todotexto:nth-of-type(3) .paragrafos p img');
+      imagem3.src = './assets/images/icon-plus.svg';
+      
+      let texto3 = document.querySelector('.todotexto:nth-of-type(3) > .paragrafos > .esconder');
+      texto3.style.display = 'none';
+ 
+       //* pressionar2
+       let imagem2 = document.querySelector('.todotexto:nth-of-type(2) .paragrafos p img');
+       imagem2.src = './assets/images/icon-plus.svg';
+       
+       let texto2 = document.querySelector('.todotexto:nth-of-type(2) > .paragrafos > .esconder');
+       texto2.style.display = 'none';
 
     
 }
